9c4aa3a10955f5876ecf0801fc380d644c241e08
Ali Hassan <alihassankp27@gmail.com>
2023-03-12 21:22:58 +0400

    update: playground app.vue

--- playground/app.vue
+++ playground/app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <FlashButton label="hello world"/>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <FlashButton label="hello world" /> <br/>
+    <FlashTypography level="sm">Hello world</FlashTypography><br/>
+    <FlashCard img="https://source.unsplash.com/800x400/?robot,hospital" img-alt="AI in 2023"
+      title="AI Revolution in Medicine"
+      description="Artificial intelligence in healthcare is an overarching term used to describe the use of machine-learning algorithms and software, or artificial intelligence (AI), to mimic human cognition in the analysis, presentation, and comprehension of complex medical and health care data. Specifically, AI is the ability of computer algorithms to approximate conclusions based solely on input data." />
   </div>
 </template>
 
