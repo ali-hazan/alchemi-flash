f75abf0bb900342e56d032e25115574381a7d441
Ali Hassan <alihassankp27@gmail.com>
2023-03-12 14:39:02 +0400

    Feature: card with shadow

---
+++ src/runtime/components/FlashCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" :src="img" :alt="imgAlt">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">{{ title }}</div>
+            <p class="text-gray-700 text-base">
+                {{ description }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        img: {
+            type: String,
+        },
+        imgAlt: {
+            type: String,
+            default: 'Image'
+        },
+        title: {
+            type: String,
+        },
+        description: {
+            type: String,
+        }
+    }
+})
+</script>
---
+++ stories/Card.stories.ts
@@ -0,0 +1,30 @@
+import FlashCard from '../src/runtime/components/FlashCard.vue';
+
+export default {
+  title: 'Card',
+  component: FlashCard,
+  tags: ['autodocs'],
+  argTypes: {
+    default: {
+      control: {
+        type: 'text',
+      },
+    }
+  },
+  render: (args: any) => ({
+    components: { FlashCard },
+    setup() {
+      return { args };
+    },
+    template: `<FlashCard v-bind="args">${args.default}</FlashCard>`,
+  }),
+};
+
+export const Main = {
+  args: {
+    img: 'https://source.unsplash.com/800x400/?robot,hospital',
+    imgAlt: 'AI in 2023',
+    title: 'AI Revolution in Medicine',
+    description: 'Artificial intelligence in healthcare is an overarching term used to describe the use of machine-learning algorithms and software, or artificial intelligence (AI), to mimic human cognition in the analysis, presentation, and comprehension of complex medical and health care data. Specifically, AI is the ability of computer algorithms to approximate conclusions based solely on input data.',
+  }
+};
